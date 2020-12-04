8026e01f0319aa8cef18fb8ca6c8fe2618dc802f
bagus <bagusirawansumitro@gmail.com>
2020-12-04 14:14:42 +0700

    add file script.js, edit navbar

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+// Resize Logo
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('img').removeClass('big');
+    $('img').addClass('small');
+  } else {
+    $('img').removeClass('small');
+    $('img').addClass('big');
+  }
+});
+
+// Resize Icon WA
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('i').removeClass('big');
+    $('i').addClass('small');
+  } else {
+    $('i').removeClass('small');
+    $('i').addClass('big');
+  }
+});
+
+// Resize Hamburger Icon
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('button').removeClass('big');
+    $('button').addClass('small');
+  } else {
+    $('button').removeClass('small');
+    $('button').addClass('big');
+  }
+});
+
+// Resize Hamburger Icon
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('span').removeClass('big');
+    $('span').addClass('small');
+  } else {
+    $('span').removeClass('small');
+    $('span').addClass('big');
+  }
+});
+
+// Resize Nav Bar
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('nav').removeClass('bg-pop');
+    $('nav').addClass('bg-pop-small');
+  } else {
+    $('nav').removeClass('bg-pop-small');
+    $('nav').addClass('bg-pop');
+  }
+});
+
+// Resize Link
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()>15){
+    $('a').removeClass('big li a');
+    $('a').addClass('small li a');
+  } else {
+    $('a').removeClass('small li a');
+    $('a').addClass('big li a');
+  }
+});
